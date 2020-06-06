d129e8f13d1578be84694a79b6369f75fb809ac9
sarthak <sarthakjaiswal45@gmail.com>
2020-06-06 19:45:30 +0530

    Updates

---
+++ precache-manifest.b1271cd1a95ea510b73b852434e1639c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b73ac6f8daa4b2cda9adce833f90854",
+    "url": "/keepers-app/index.html"
+  },
+  {
+    "revision": "3a91a3cf97e56af565bf",
+    "url": "/keepers-app/static/js/2.c0319bad.chunk.js"
+  },
+  {
+    "revision": "9d052a3b4cc19d113d0b",
+    "url": "/keepers-app/static/js/main.8200c2c1.chunk.js"
+  },
+  {
+    "revision": "81250efd5e49f4e17e0b",
+    "url": "/keepers-app/static/js/runtime-main.3e7383ad.js"
+  }
+]);
